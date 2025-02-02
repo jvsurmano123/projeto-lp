92deaf200ddf0a11543d79cedafca54292c8ebce
jvsurmano123 <jvsurmano123@gmail.com>
2025-02-02 15:01:49 -0300

    Update Supabase config to use environment variables

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qkmayhehanpazqeoafmh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrbWF5aGVoYW5wYXpxZW9hZm1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1MTcxNTYsImV4cCI6MjA1NDA5MzE1Nn0.A6NMKnMm_YyVtG5TtHIEPpMK8trt33OsZv2hnr3xhW0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
